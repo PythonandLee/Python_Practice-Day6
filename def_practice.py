cbdc381c5d9d980b48f7966c539d154ff5cde80a
PythonandLee <47309701+PythonandLee@users.noreply.github.com>
2019-02-12 22:17:16 -0400

    Delete def_practice.py

--- def_practice.py
+++
@@ -1,6 +0,0 @@
-def describe_city(city = 'rio', country = 'brazil'):
-    print(city.title() + " is in " + country.title() + ".")
-
-describe_city()
-describe_city(city = 'new york', country = 'the united state')
-describe_city(country = 'philippines', city = 'cebu')
